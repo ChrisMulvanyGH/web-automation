37e1f4c0473288ad26c5d9736132b40af6256943
Chris Mulvany <info@chrismulvany.com>
2022-08-19 20:44:35 +0100

    Added URL to goto in scrape.js

--- funcs/scrape.js
+++ funcs/scrape.js
@@ -2,11 +2,25 @@ const scrapeData = async () => {
     // Import the launchChrome() from 'browser.js'
     const { launchChrome } = require('./browser');
 
+    // Launcging Chrome and Opening a new Tab/Page
     const [newPage, exitChrome] = await launchChrome();
     const [page] = await newPage;
-    if(!page) return;
+    if(!page) return;  // Exit if the page has not opened properly
 
-    await exitChrome();
+    // Visit a website url
+    const url = 'http://www.domain.com';
+    console.log('Opening ' + url );
+    try {
+        await page.goto(url, {
+            waitUntil: 'networkidle0' // Wait until all network requests have been processed
+        });
+    } catch(e) {
+        console.log("Unable to visit " + url, e);
+        await exitChrome(); // Close chrome on error
+        return; // Exit the function
+    }
+
+    await exitChrome(); // Close Chrome
 };
 
 module.exports = scrapeData;
