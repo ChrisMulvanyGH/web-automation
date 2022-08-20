c9114ce0305a1847c175615798bed4595d82b2a2
Chris Mulvany <info@chrismulvany.com>
2022-08-20 17:57:48 +0100

    Collecting Genres and calling in index.js

---
+++ browser.js
@@ -0,0 +1,35 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    'use strict'
+
+    const url = 'https://www.goodreads.com/choiceawards/best-books-2020';
+    const browser = await puppeteer.launch({ headless: false, defaultViewport: null });
+    const page = await browser.newPage();
+    console.log('Opening ' + url);
+    try {
+        await page.goto(url, {
+            waitUntil: 'networkidle0',
+        });
+    } catch(err) {
+        console.error('Unable to visit: ' + url, err);
+        await browser.close();
+    }
+
+        
+    const genres = await page.evaluate(() => {
+        const genre = document.querySelectorAll('.category.clearFix');
+        const allGenres = [];
+
+        for(let i=0; i<genre.length; i++) {
+            allGenres.push({
+                title: genre[i].querySelector('h4').innerText,
+                link: genre[i].querySelector('a[href').href
+            })
+        }
+        return allGenres;
+    });
+    console.log(genres);
+
+    await browser.close();    
+})();
---
+++ index.js
@@ -0,0 +1,2 @@
+const browser = require('./browser');
+browser;
